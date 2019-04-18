b347e08d9cfcbb8eb44f95469a518f8a1a5bdfc4
Levi Cook <levicook@gmail.com>
2019-04-17 22:10:53 -0600

    more code organization

--- grpc-intro/demos/echo-demo/backend/go.mod
+++ grpc-intro/demos/echo-demo/backend/go.mod
@@ -1,3 +1,7 @@
-module echo-backend
+module backend
 
-require google.golang.org/grpc v1.20.0
+require (
+	github.com/golang/protobuf v1.3.1
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.20.0
+)
--- grpc-intro/demos/echo-demo/backend/main.go
+++ grpc-intro/demos/echo-demo/backend/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	_ "google.golang.org/grpc"
+	"backend/api"
 )
 
 func main() {
-	ticker := time.NewTicker(500 * time.Millisecond)
-
-	go func() {
-		for t := range ticker.C {
-			fmt.Println("Tick at", t)
-		}
-	}()
-
-	time.Sleep(30 * time.Second)
-	ticker.Stop()
-	fmt.Println("Ticker stopped")
+	_ = api.EchoMessage{}
+	println("hello world")
 }
